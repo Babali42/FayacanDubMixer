7bcc3f992bb8e24e6fac874661084cd48918b3d8
Babali42 <bab07ali@gmail.com>
2021-08-20 14:16:21 +0000

    chargement des sons

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,36 @@ const main = async () => {
   game.createScene();
   game.run();
 
-  const button = document.getElementById('close-modal');
+  const buttonStart = document.getElementById('close-modal');
+  const buttonCredit = document.getElementById('credit-modal-button');
+  const buttonCloseCredit = document.getElementById('close-credit-modal-button');
 
-  button.addEventListener('click', event => {
+
+  buttonStart.addEventListener('click', event => {
     console.log("zbeul");
   });
+
+  buttonCredit.addEventListener('click', event => {
+    toggleCreditModal();
+  });
+
+  buttonCloseCredit.addEventListener('click', event => {
+    toggleCreditModal();
+  })
 }
 
 main().catch(err => {
   console.error(err);
 });
+
+function toggleCreditModal(){
+  var creditModal = document.getElementById("creditModal");
+    var startModal = document.getElementById("startModal");
+    if (creditModal.style.display === "none") {
+        creditModal.style.display = "flex";
+        startModal.style.display = "none";
+    } else {
+      creditModal.style.display = "none";
+      startModal.style.display = "table-cell";
+    }
+}
--- src/services/sound.ts
+++ src/services/sound.ts
@@ -1,10 +1,62 @@
 import { Service } from "typedi";
+import * as Pizzicato from "pizzicato";
 
 @Service()
 class SoundService{
+    numberLoaded = 0;
+    numberTotalToLoad = 13;
+    pist_0_Transition : Pizzicato.sound;
+    pist_1_kick :Pizzicato.Sound;
+    pist_1_1_kick_fx :Pizzicato.Sound;
+    pist_2_snare :Pizzicato.Sound;
+    pist_2_snare_fx :Pizzicato.Sound;
+    pist_3_hat :Pizzicato.Sound;
+    pist_3_1_hat_fx :Pizzicato.Sound;
+    pist_4_toms :Pizzicato.Sound;
+    pist_5_bass :Pizzicato.Sound;
+    pist_6_sub_bass :Pizzicato.Sound;
+    pist_7_skank :Pizzicato.Sound;
+    pist_7_1_skank_fx :Pizzicato.Sound;
+    pist_8_melodies :Pizzicato.Sound;
+
     Log(message:string): void{
-        console.log(message);
+        this.pist_0_Transition = this.LoadSound("0_transitions");
+        this.pist_1_kick = this.LoadSound("1_kick");
+        this.pist_1_1_kick_fx = this.LoadSound("1_1_kick_fx");
+        this.pist_2_snare = this.LoadSound("2_snare");
+        this.pist_2_snare_fx = this.LoadSound("2_1_snare_fx");
+        this.pist_3_hat = this.LoadSound("3_hat");
+        this.pist_3_1_hat_fx = this.LoadSound("3_1_hat_fx");
+        this.pist_4_toms = this.LoadSound("4_toms");
+        this.pist_5_bass = this.LoadSound("5_bass");
+        this.pist_6_sub_bass = this.LoadSound("6_sub_bass");
+        this.pist_7_skank = this.LoadSound("7_skank");
+        this.pist_7_1_skank_fx = this.LoadSound("7_1_skank_fx");
+        this.pist_8_melodies = this.LoadSound("8_melodies");
     }
+
+    LoadSound(fileName: string) : Pizzicato.Sound{
+        return new Pizzicato.Sound({source: 'file',
+        options: { path: './sounds/' + fileName + '.mp3', loop: true }}, x => {
+            this.loadIncrease();
+        });
+    }
+
+    private loadIncrease() : void{
+        this.numberLoaded++;
+      
+        document.getElementById('number-sounds-loaded').innerHTML = (Math.trunc(this.numberLoaded * 100 / this.numberTotalToLoad)).toString();
+        if(this.numberLoaded >= this.numberTotalToLoad){
+            console.log("yes c'est frais");
+          var button = document.getElementById('close-modal');
+          //button.disabled = false;
+          button.classList.remove("button-disabled");
+          var button = document.getElementById('tuto-modal');
+          //button.disabled = false;
+          button.classList.remove("button-disabled");
+          //soundSetting();
+        }
+      }
 }
 
 export default SoundService;
